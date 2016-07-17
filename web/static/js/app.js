42892221faaa0d57c7a0d8ab1f5ff64c86aab537
Andre Stephano <andrestephano@gmail.com>
2016-07-17 17:05:44 -0300

    first version

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -19,3 +19,13 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+import "phoenix_html"
+
+// Import local files
+//
+// Local files can be imported directly using relative
+// paths "./socket" or full ones "web/static/js/socket".
+
+import socket from "./socket"
+import Gossip from "./gossip"
+Gossip.init(socket)
