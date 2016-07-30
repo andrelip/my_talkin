697ec621b373645f30419f04a988fd706a35fb1b
Andre Stephano <andrestephano@gmail.com>
2016-07-30 00:10:34 -0300

    added auth flow

--- web/controllers/api/facebook/auth_controller.ex
+++ web/controllers/api/facebook/auth_controller.ex
@@ -1,9 +1,60 @@
 defmodule Talkin.API.Facebook.AuthController do
   use Talkin.Web, :controller
 
+  alias Talkin.User
+
   def login(conn, %{"access_token" => access_token}) do
-    access_token
-    |> Poison.encode!
-    |> render(access_token: access_token)
+    find_or_create_user_by_access_token(access_token: access_token)
+    |> response(conn)
+  end
+
+  defp find_or_create_user_by_access_token(%{access_token: access_token}) do
+    user = Repo.get_by(User, access_token: access_token)
+    if user do
+      {:found, user}
+    else
+      find_by_uid_or_create(access_token: access_token)
+    end
+  end
+
+  defp find_by_uid_or_create(%{access_token: access_token}) do
+    response = HTTPotion.get "httpbin.org/get#{access_token}"
+    if HTTPotion.Response.success?(response) do
+      %HTTPotion.Response{content: user_params} = response
+      |> find_by_uid_or_create(user_params: user_params)
+    else
+      %HTTPotion.Response{content: user_params} = response
+      |> {:error, "Facebook: #{content}"}
+    end
+  end
+
+  defp find_by_uid_or_create(%{user_params: user_params}) do
+    user = Repo.get_by(User, uid: user_params[:uid])
+    if user do
+      {:changed_access_token, user}
+    else
+      create_user(user_params: user_params)
+    end
+  end
+
+  defp create_user(%{user_params: user_params}) do
+    # user_params =
+    changeset = User.changeset(%User{}, user_params)
+
+    case Repo.insert(changeset) do
+      {:created, user} ->
+        conn
+        |> render(user)
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  defp response({:error, changeset}, conn) do
+  end
+
+  defp response({_, user}, conn) do
+    conn
+    |> render(user)
   end
 end
